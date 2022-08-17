46a1cf44bf4365bf8b5202c4570cd800d4319808
kanedaaaa <samflynn19121@gmail.com>
2022-08-17 10:32:10 +0400

    feat/ add multisig

--- contracts/Deployer.sol
+++ contracts/Deployer.sol
@@ -3,6 +3,8 @@
 pragma solidity ^0.8.11;
 
 import "./ERC-Templates/TERC721.sol";
+import "./ERC-Templates/TERC20.sol";
+import "./ERC-Templates/TMultiSig.sol";
 
 /**
 * Deployer contract. Will deploy template ERC's with 
@@ -15,6 +17,8 @@ contract Deployer {
     mapping(address => address[]) private contractByUser;
 
     event genericERC721Deployer(address _deployer, address _contract);
+    event genericERC20Deployer(address _deployer, address _contract);
+    event multisigDeployer(address _deployer, address _contract);
 
     /**
     * @notice generic ERC721 stands for ERC721Enumerable
@@ -41,6 +45,37 @@ contract Deployer {
         );
         contractByUser[msg.sender].push(address(terc));
         emit genericERC721Deployer(msg.sender, address(terc));
+    }
+
+    function genericERC20( 
+        string memory _name,
+        string memory _symbol,
+        uint256 _totalSupply,
+        bool _cap,
+        bool _burn
+    ) public {
+        TERC20 terc = new TERC20(
+            _name,
+            _symbol,
+            _totalSupply,
+            _cap,
+            _burn
+        );
+        contractByUser[msg.sender].push(address(terc));
+        emit genericERC20Deployer(msg.sender, address(terc));
+    }
+
+    function MultiSig(
+        address[] memory _owners, 
+        uint256 _numConfirmationsRequired
+    ) public {
+        TMultiSig tmultisig = new TMultiSig(
+            _owners,
+            _numConfirmationsRequired
+        );
+
+        contractByUser[msg.sender].push(address(tmultisig));
+        emit multisigDeployer(msg.sender, address(tmultisig));
     }
 
     /**
